7da31788ae9d80f9c622e0efb49a1e0b4e373bd3
Goran Belanovic <goranbelanovic@yahoo.com>
2021-03-21 03:14:35 +0100

    msg

--- routes/publishArticle.js
+++ routes/publishArticle.js
@@ -4,7 +4,11 @@ const Article = require('../models/Article');
 
 router.put('/publishArticle/:id', async (req, res) => {
     try {
-        const article = await Article.findByIdAndUpdate(req.params.id, {published: true}, {new: true});
+        const article = await Article.findByIdAndUpdate(
+            req.params.id, 
+            {published: req.body.published, datePublished: req.body.datePublished},
+            {new: true}
+        );
         res.status(200).send(article);
     }
     catch(err){
