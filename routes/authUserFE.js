dba9197aa51c240584a0bc2d17b9e24540a0b041
Goran Belanovic <Goran Belanovic>
2021-04-26 23:02:33 +0200

    'msg'

--- routes/authUserFE.js
+++ routes/authUserFE.js
@@ -9,11 +9,14 @@ router.post('/authOneUserFE', async (req, res) => {
     const { error } = validateUserAuth(_.pick(req.body, ['username', 'password']));
     if (error) res.status(400).send({ validate_error: error.details[0].message });
 
-    const result1 = await UserFrontend.findOne({ username: req.body.username });
-    if (!result1) res.status(400).send({ validate_error: `Invalid username or password`});
+    const resultUsername = await UserFrontend.findOne({ username: req.body.username });
+    if (!resultUsername) res.status(400).send({ validate_error: `Invalid username or password`});
     
-    const result2 = await UserFrontend.findOne({ password: req.body.password });
-    if (!result2) res.status(400).send({ validate_error: `Invalid username or password`});
+   /*  const result2 = await UserFrontend.findOne({ password: req.body.password });
+    if (!result2) res.status(400).send({ validate_error: `Invalid username or password`}); */
+
+    const resultPassword = await bcrypt.compare(req.body.password, resultUsername.password);
+    if (!resultPassword) res.status(400).send({ validate_error: `Invalid username or password`});
 
     try {
         let msg = ['logged in', true]
