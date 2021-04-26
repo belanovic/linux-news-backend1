8bfc6d32a1eea24f24ce3d7ff5814d3aec491dca
Goran Belanovic <Goran Belanovic>
2021-04-26 02:33:07 +0200

    'msg'

--- models/UserFrontend.js
+++ models/UserFrontend.js
@@ -31,4 +31,16 @@ const userFrontendSchema = new mongoose.Schema({
 
 const UserFrontend = mongoose.model('UserFrontend', userFrontendSchema);
 
-module.exports = UserFrontend;
+function validateUser(user) {
+    const schema = {
+        firstName: Joi.string().min(3).max(50).required(),
+        lastName:  Joi.string().min(3).max(50).required(),
+        username:  Joi.string().min(3).max(50).required(),
+        password:  Joi.string().min(6).max(255).required(),
+        email:  Joi.string().min(3).max(60).email().required()
+    }
+    Joi.validate(user, schema);
+}
+
+module.exports.UserFrontend = UserFrontend;
+module.exports.validateUser = validateUser;
--- routes/oneUserFE.js
+++ routes/oneUserFE.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const UserFrontend = require('../models/UserFrontend');
+const {UserFrontend, } = require('../models/UserFrontend');
 
 router.post('/oneUserFE', async (req, res) => {
 
