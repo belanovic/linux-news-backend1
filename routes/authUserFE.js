ff87914000cd6be464ec5d92f3d9d723e16fbad3
Goran Belanovic <Goran Belanovic>
2021-04-26 21:54:17 +0200

    'msg'

--- routes/authUserFE.js
+++ routes/authUserFE.js
@@ -10,10 +10,10 @@ router.post('/authOneUserFE', async (req, res) => {
     if (error) res.status(400).send({ validate_error: error.details[0].message });
 
     const result1 = await UserFrontend.findOne({ username: req.body.username });
-    if (!result) res.status(400).send({ validate_error: `Invalid username or password`});
+    if (!result1) res.status(400).send({ validate_error: `Invalid username or password`});
     
     const result2 = await UserFrontend.findOne({ password: req.body.password });
-    if (!result) res.status(400).send({ validate_error: `Invalid username or password`});
+    if (!result2) res.status(400).send({ validate_error: `Invalid username or password`});
 
     try {
         let msg = ['logged in', true]
