9509855ba2dcaee6e6051dd122aacac7a37ef738
Goran Belanovic <goranbelanovic@yahoo.com>
2021-04-28 02:18:52 +0200

    'msg'

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ const config = require('config');
 
 
 function auth(req, res, next) {
+    console.log(req.header('x-auth-token'));
     const token = req.header('x-auth-token');
     if (!token) return res.status(401).send({token_error: 'Access denied. No token provided'});
 
