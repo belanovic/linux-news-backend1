09095a079d0231b40f75744cd48efc02f13f650a
Goran Belanovic <goranbelanovic@yahoo.com>
2021-05-01 02:56:29 +0200

    'msg'

--- routes/oneUserFE.js
+++ routes/oneUserFE.js
@@ -34,7 +34,8 @@ router.post('/oneUserFE', async (req, res) => {
 
     try {
         const savedOneUser = await oneUser.save();
-        let msg = [true, 'registration_successfull', savedOneUser, token]
+        let msg = [true, 'registration_successfull', savedOneUser, token];
+        res.json(msg);
     }
     catch (error) {
         let msg = [false, 'registration_error', error]
