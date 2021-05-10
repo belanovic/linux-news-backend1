f0f074df64e13969e596e637f342cf2954fa520e
Goran Belanovic <goranbelanovic@yahoo.com>
2021-05-11 00:10:23 +0200

    'msg'

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ mongoose.connect(mongoAdress2, { useNewUrlParser: true, useUnifiedTopology: true
 
 const port = process.env.PORT || 4000;
 process.env.TZ = "Europe/Belgrade";
+console.log('ovo je vreme: ' + new Date()) 
 
 
 server.listen(port, HOST_BACKEND, () => console.log(`Server is listening on port ${port}`));
