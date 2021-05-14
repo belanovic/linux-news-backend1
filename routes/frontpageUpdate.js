6330800e38069b97b7f6ef5b23da3b3ae920db2f
Goran Belanovic <goranbelanovic@yahoo.com>
2021-05-15 01:57:54 +0200

    'msg'

--- routes/frontpageUpdate.js
+++ routes/frontpageUpdate.js
@@ -9,6 +9,7 @@ router.put('/updateFrontpage', async (req, res) => {
         const allArticles = await Article.find();
         const modifiedAllArticles = allArticles.map((prom) => {
             const idAndPositionMatch = req.body.idAndPositionArr.filter((idAndPosition) => {
+                console.log(idAndPosition + " i " + prom)
                 return idAndPosition.id === prom._id
             });
             console.log(idAndPositionMatch)
