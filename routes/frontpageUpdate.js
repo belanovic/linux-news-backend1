8e5dbd27e8ec48bbce53802946e00349be44c425
Goran Belanovic <goranbelanovic@yahoo.com>
2021-05-14 23:22:56 +0200

    'msg'

--- routes/frontpageUpdate.js
+++ routes/frontpageUpdate.js
@@ -6,7 +6,10 @@ router.put('/updateFrontpage', async (req, res) => {
     try {
         console.log('evo ga');
         const allArticles = await Article.find();
-        const modifiedAllArticles = allArticles.map((prom) => prom.position = 0);
+        const modifiedAllArticles = allArticles.map((prom) => {
+            prom.position = 0;
+            return prom;
+        })
 
         /* const article = await Article.findByIdAndUpdate(req.params.id, {position: req.body.position}, {new: true}); */
         res.status(200).send(modifiedAllArticles);
