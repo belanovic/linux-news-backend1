571cf042b4cdf12dc80cc7cf1c7cb7811f9dd105
Goran Belanovic <goranbelanovic@yahoo.com>
2021-05-15 03:41:59 +0200

    'msg'

--- routes/frontpageUpdate.js
+++ routes/frontpageUpdate.js
@@ -8,17 +8,15 @@ router.put('/updateFrontpage', async (req, res) => {
         const allArticles = await Article.find();
         const modifiedAllArticles = allArticles.map((prom) => {
             const idAndPositionMatch = req.body.idAndPositionArr.filter((idAndPosition) => {
-                console.log(idAndPosition.id === prom._id.toString())
                 return idAndPosition.id === prom._id.toString()
             });
-            console.log(idAndPositionMatch)
             const newArticlePosition = idAndPositionMatch.length > 0? parseInt(idAndPositionMatch[0].newPosition) : 0;
            /*  const modifiedArticle = Object.assign({}, prom);
             modifiedArticle.position = newArticlePosition;
             return modifiedArticle; */
             const modifiedArticle = prom;
             modifiedArticle.position = newArticlePosition;
-            return modifiedArticle;
+            return modifiedArticle.sort((a, b) => a.position - b.position);
         })
 
         /* const article = await Article.findByIdAndUpdate(req.params.id, {position: req.body.position}, {new: true}); */
