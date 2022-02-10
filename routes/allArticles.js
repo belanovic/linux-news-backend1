8ba50127c270bf77e4f481c593983585f7f517a0
Goran Belanovic <goranbelanovic@yahoo.com>
2022-02-10 18:08:38 +0100

    'msg'

--- routes/allArticles.js
+++ routes/allArticles.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Article = require('../models/Article');
-const auth = require('../middleware/auth');
+const auth = require('../middleware/auth');  
 
-router.get('/allArticles', auth, async (req, res) => {
+router.get('/allArticles', auth, async (req, res) => {  
     try {
         const articles = await Article
             .find() 
