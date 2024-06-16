ddc39c9d5dae2e19971914cb0d6941557ea0369d
belanovic <goranbelanovic@yahoo.com>
2024-06-16 19:49:41 +0200

    msg

--- startup/headers.js
+++ startup/headers.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
         /* if((req.headers.origin && (req.headers.origin.includes('http://localhost')) || req.headers.origin == 'https://linux-news-cms.onrender.com')) {
             res.setHeader('Access-Control-Allow-Origin', req.headers.origin); 
         } */
-        res.setHeader('Access-Control-Allow-Origin', req.headers.origin); 
+        
         // Website you wish to allow to connect
 
         //  res.setHeader('Access-Control-Allow-Origin', '*'); 
