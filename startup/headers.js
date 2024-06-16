24f0b5a376353270d719010abc06d669843b3fae
belanovic <goranbelanovic@yahoo.com>
2024-06-16 20:07:08 +0200

    msg

--- startup/headers.js
+++ startup/headers.js
@@ -32,7 +32,7 @@ module.exports = function (app) {
         // Request headers you wish to allow
         /* res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type'); */
         res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-        res.setHeader('Access-Control-Allow-Origin', 'https://linux-news-cms.onrender.com/'); 
+        res.setHeader('Access-Control-Allow-Origin', 'https://linux-news-cms.onrender.com'); 
         
         // Set to true if you need the website to include cookies in the requests sent
         // to the API (e.g. in case you use sessions)
