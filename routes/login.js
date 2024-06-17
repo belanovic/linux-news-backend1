8ee6afe724aef11e77ec29ad9a386a4fe4726835
belanovic <goranbelanovic@yahoo.com>
2024-06-18 01:24:55 +0200

    msg

--- routes/login.js
+++ routes/login.js
@@ -32,7 +32,7 @@ router.post('/login', async (req, res) => {
         /* const token = jwt.sign(userData, config.get('jwtPrivateKey'), {expiresIn: '55m'}); */
         const token = userRegistered.generateToken();
         
-        res.cookie('token', token, {httpOnly: false, sameSite: 'strict', secure: true})
+        res.cookie('token', token, {httpOnly: false, sameSite: 'none', secure: true})
         return res.json({loginMsg: new LoginMsg(true, userRegistered)})
 
     } catch (error) {
