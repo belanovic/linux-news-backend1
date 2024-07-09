6179d32902ca56327d06788f7100a804250089a5
belanovic <goranbelanovic@yahoo.com>
2024-07-09 23:56:19 +0200

    msg

--- startup/db.js
+++ startup/db.js
@@ -6,9 +6,8 @@ module.exports = function() {
     const mongoAddress1 = `mongodb://localhost/news`;
     const mongoAddress2 = `mongodb+srv://goranbelanovic:1234@cluster0.xneom.mongodb.net/news?retryWrites=true&w=majority`;
 
-    mongoose.set('useFindAndModify', false);
     
-    mongoose.connect(mongoAddress2, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(mongoAddress2)
         .then(() => console.log('Connected to the news database'))
         .catch(error => console.log(error.message))
 }
